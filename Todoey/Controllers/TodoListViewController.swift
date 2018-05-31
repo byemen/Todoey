80ce4fcbfa0a1d02b59ef428189209f6b68a2b27
byemen <byemen@gmail.com>
2018-05-31 22:20:49 +1000

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -14,27 +14,14 @@ class TodoListViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
     let arrayKeyName:String = "TodoListArray"
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     @IBOutlet var todoListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let newItem = Item()
-        newItem.title = "Find Mike1"
-        itemArray.append(newItem)
-
-        let newItem1 = Item()
-        newItem1.title = "Find Mike2"
-        itemArray.append(newItem1)
-
-        let newItem2 = Item()
-        newItem2.title = "Find Mike3"
-        itemArray.append(newItem2)
-
-        if let items = defaults.array(forKey: arrayKeyName) as? [Item] {
-            itemArray = items
-        }
+        loadItems()
     }
     
     //MARK - Tableview Datasource Methods.
@@ -58,6 +45,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true) // fade out of the grey color of cell as soon as tab the cell.
         tableView.reloadData()
     }
@@ -74,7 +62,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: self.arrayKeyName)
+            self.saveItems()
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
@@ -83,6 +71,31 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK - Model Manupulation Method
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to:dataFilePath!)
+        } catch {
+            print("Error!")
+        }
+
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("Error \(error)")
+            }
+        }
     }
 }
 
