0623442e60282207bc81bd8382bc7512721a526e
byemen <byemen@gmail.com>
2018-05-31 16:03:16 +1000

    Added tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) Func at TodoListViewController.swift

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -21,7 +21,6 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
     
@@ -33,7 +32,20 @@ class TodoListViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+//        print(cell?.textLabel?.text! ?? "tableView.cellForRow")
+        if cell?.accessoryType == UITableViewCellAccessoryType.checkmark {
+            cell?.accessoryType = .none
+        }else{
+            cell?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) // fade out of the grey color of cell as soon as tab the cell.
+//        print()
+    }
 }
 
